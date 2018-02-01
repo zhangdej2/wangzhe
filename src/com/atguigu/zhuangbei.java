ce9ca4c13e35a640abcfc3489493dd8a5d5e5687
狄仁杰 <707731199@qq.com>
2018-02-01 17:30:58 +0800

    测试补充

--- src/com/atguigu/zhuangbei.java
+++ src/com/atguigu/zhuangbei.java
@@ -6,6 +6,8 @@ public class zhuangbei {
 		System.out.println("创建文件");
 		System.out.println("更新测试1");
 		System.out.println("更新测试2");
+		System.out.println("AAAAAAAAA");
+		System.out.println("BBBBBBBBB");
 	}
 
 }
